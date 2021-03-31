d0c9d39aa3472f3420883bfda51c13b623ec56e9
johnedquinn <johnedquinn@gmail.com>
2021-03-31 19:05:30 -0400

    Initial import of local dir

---
+++ start-n1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+geth --datadir ./node01 \
+	--identity "jq-n1" \
+	--http \
+	--http.port 8000 \
+	--http.api "eth,net,web3,personal,miner,admin" \
+	--http.corsdomain "*" \
+	--port 30303 \
+	--networkid 15 \
+	--nat "any"
---
+++ start-n2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+geth --datadir ./node02 \
+	--identity "jq-n2" \
+	--http \
+	--http.port 8001 \
+	--http.api "eth,net,web3,personal,miner,admin" \
+	--http.corsdomain "*" \
+	--port 30304 \
+	--networkid 15 \
+	--nat "any"
